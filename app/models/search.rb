afe7a1c7e097c63964395609b7334e6ff19a41b2
lmaisour <lmaisour@gmail.com>
2014-08-31 15:39:42 -0700

    corrected validation for search

--- app/models/search.rb
+++ app/models/search.rb
@@ -1,6 +1,6 @@
 class Search < ActiveRecord::Base
   belongs_to :hashtag_feed
 
-  validates :name, presence: true
+  validates :topic, presence: true
   validates :hashtag_feed_id, presence: true
 end
